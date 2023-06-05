a670a3ba70ff744e4e12c47a974a34a1ce5244eb
mabroukatriki <mabrouka.triki@lmdsio.fr>
2023-06-05 14:31:57 +0200

    last modification

--- index.php
+++ index.php
@@ -1,45 +1,37 @@
 <?php
 /*
-Template Name: Home Template
+Template Name: Index Template
 */
 ?>
-
 <?php get_header(); ?>
 
-<!-- Contenu de la page -->
-<section class="container d-flex align-items-center justify-content-center my-5">
-    <div class="row">
-        <div class="col-md-6">
-            <div class="bg-transparent p-3 transparent-bg">
-                <h1 class="display-4 text-white title">Taste the authentic Saudi cuisine</h1>
-                <p class="lead text-white">Among the best Saudi chefs in the world, serving you something beyond flavor.</p>
-            </div>
+<section class="container">
+  <div class="row">
+    <?php
+    $args = array(
+      'post_type' => 'post',
+      'posts_per_page' => 5 // Adjust the number of posts to display here
+    );
+
+    $query = new WP_Query($args);
+
+    if ($query->have_posts()) {
+      while ($query->have_posts()) {
+        $query->the_post();
+        // Display post content here
+        ?>
+        <div class="col-md-4">
+          <h2><?php the_title(); ?></h2>
+          <div class="post-content">
+            <?php the_excerpt(); ?>
+          </div>
         </div>
-        <div class="col-md-6">
-            <img src="<?php echo get_template_directory_uri(); ?>/image/photo.png" alt="Image de droite" class="img-fluid">
-        </div>
-    </div>
-</section>
-
-<!-- Autres sections et contenu de la page -->
-
-<section class="container-fluid full-screen-image position-relative">
-    <img src="<?php echo get_template_directory_uri(); ?>/image/photo.png" alt="Full Screen Image" class="img-fluid w-100 mw-100">
-    <div class="image-content position-absolute top-50 start-50 translate-middle text-center">
-        <h1 class="display-4 text-white">A unique menu that reflects the true essence of Saudi cuisine</h1>
-        <a href="<?php echo get_permalink(get_page_by_path('menu')); ?>" class="btn btn-transparent custom-btn" style="background-color: #CC9D2F;">Our Menu</a>
-    </div>
-</section>
-
-<?php
-    // Boucle WordPress pour afficher le contenu
-    while (have_posts()) {
-        the_post();
-        the_content();
+        <?php
+      }
+      wp_reset_postdata();
     }
-?>
+    ?>
+  </div>
+</section>
 
 <?php get_footer(); ?>
-<?php wp_footer(); ?>
-</body>
-</html>
