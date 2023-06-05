bd42f5a4c757966a372ac4ce743de3d934685e40
mabroukatriki <133276394+mabroukatriki@users.noreply.github.com>
2023-06-05 21:23:35 +0200

    Delete framework directory

--- framework/helpers/options/start.php
+++
@@ -1,8 +0,0 @@
-<div class="theme-options-group">
-    <table cellspacing="0" class="widefat options-table">
-        <thead>
-            <tr>
-                <th scope="row" colspan="2"><?php echo $name; ?></th>
-            </tr>
-        </thead>
-        <tbody>
